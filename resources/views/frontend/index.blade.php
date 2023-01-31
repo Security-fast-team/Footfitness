e6c425353ba10d3fb578914d5dbb2fe9586fa0fb
Akash Debnath <akash93734089@gmail.com>
2023-01-31 22:00:58 +0600

    html structure

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,9 +2,139 @@
 @push('custom-css')
 @endpush
 @section('page_conent')
-    <h2>Bangladesh</h2>
 
+<!-- HEADER-SECTION-START -->
+<div class="header-section">
+    <div class="container">
+        <h1>চুল পড়া নিয়ে চিন্তিত ?</h1>
+    </div>
+</div>
+<!-- HEADER-SECTION-END -->
 
+<!-- VIDEO-SECTION-START -->
+<div class="header-section">
+    <div class="container">
+        <h2>চুলের যে কোন সমস্যার প্রকৃতিক সমাধান পেতে <span>ভিডিওটি শেষ পর্যন্ত দেখুন..</span></h2>
+        <div class="video">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/QvbuDAaaQ6Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        <div class="order-btn">
+            <a href="এখানে অর্ডার করুন"></a>
+        </div>
+    </div>
+</div> 
+<!-- VIDEO-SECTION-END -->
+
+<!-- BENEFIT-SECTION-START -->
+<div class="benefit-section">
+    <div class="container">
+        <h2 class="title">কেন এই কম্বো আপনার <span>ব্যবহার করা উচিত?</span></h2>
+        <div class="benefit-col-group">
+            <div class="benefit-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            </div>
+            <div class="benefit-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            </div>
+            <div class="benefit-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            </div>
+            <div class="benefit-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            </div>
+            <div class="benefit-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            </div>
+            <div class="benefit-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            </div>
+            <div class="benefit-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            </div>
+            <div class="benefit-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            </div>
+            
+        </div>
+
+        <div class="order-btn">
+            <a href="এখানে অর্ডার করুন"></a>
+        </div>
+    </div>
+</div>
+<!-- BENEFIT-SECTION-END -->
+
+<!-- qualitative-elements-SECTION-START -->
+<div class="qualitative-section">
+    <div class="container">
+        <h2 class="title">যেসব গুনগত উপাদান দিয়ে এই <span>কম্বো তৈরী ?</span></h2>
+        <div class="qualitative-col-group">
+            <div class="qualitative-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>আমলকি</h2></div>
+            </div>
+            <div class="qualitative-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>আমলকি</h2></div>
+            </div>
+            <div class="qualitative-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>আমলকি</h2></div>
+            </div>
+            <div class="qualitative-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>আমলকি</h2></div>
+            </div>
+            <div class="qualitative-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>আমলকি</h2></div>
+            </div>
+            <div class="qualitative-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>আমলকি</h2></div>
+            </div>
+            <div class="qualitative-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>আমলকি</h2></div>
+            </div>
+            <div class="qualitative-item">
+                <div class="item-img"></div>
+                <div class="item-text"><h2>আমলকি</h2></div>
+            </div>
+        </div>
+            <div class="extra-title">
+                <h2>এইরকম ৪২টি উপাদান ব্যবহার করা হয় আমাদের এই কম্বোটি তৈরী করতে।</h2>
+            </div>
+        <div class="order-btn">
+            <a href="এখানে অর্ডার করুন"></a>
+        </div>
+    </div>
+</div>
+<!-- qualitative-elements-SECTION-END -->
+
+<!-- PRICE-SECTION-START -->
+<div class="price-section">
+    <div class="container">
+        <div class="main">
+            <h2>অফার মূল্য ​</h2>
+            <h3>৩টি বোতল একসাথে পাচ্ছেন ,</h3>
+            <h3>৳ ৯৫০/- <span><del>৳১৪৯৯/-</del></span></h3>
+            <h2>--ডেলিভারি চার্জ প্রযোজ্য--</h2>
+            <h3>সারাদেশে ক্যাশ অন হোম ডেলিভারি দেয়া হয়</h3>
+            <div class="order-btn">
+            <a href="এখানে অর্ডার করুন"></a>
+        </div>
+        </div>
+    </div>
+</div>
 @endsection
 @push('custom-js')
 
