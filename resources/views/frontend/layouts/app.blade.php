4ba6bd6ce9785a3b964fb1ea530373bdac5b1ec1
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-01-31 12:41:16 +0600

    fixing

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -18,53 +18,7 @@
 
 
 
-    {{-- ================================================================================================
-        =============================================  javascript  global function ========================
-        =================================================================================================  --}}
-    <script>
-        // ========================Language conversation
-        var bn2en = n => n.replace(/[০-৯]/g, d => "০১২৩৪৫৬৭৮৯".indexOf(d));
-        var en2bn = n => n.replace(/\d/g, d => "০১২৩৪৫৬৭৮৯" [d]);
-
-
-        //  ===================Toaster object
-        var toastr = {
-            count: 0,
-            toaster_append(msg, class_name) {
-                $('.toastr-div').append(`<div class="toastr ${class_name} mt-2">
-                                                    <span class="toaster-msg">${msg}</span>
-                                                </div>`);
-            },
-            success(msg) {
-                let class_name = 'toastr' + this.count;
-                this.toaster_append(msg, class_name);
-
-                $('.' + class_name).fadeIn(1000);
-                setTimeout(() => {
-                    $('.' + class_name).fadeOut(1000);
-                }, 2500);
-                this.count++;
-            },
-            error(msg) {
-                let class_name = 'toastr' + this.count;
-                this.toaster_append(msg, class_name);
-
-                $('.' + class_name).css({
-                    backgroundColor: 'red'
-                });
-                $('.' + class_name).fadeIn(1000);
-                setTimeout(() => {
-                    $('.' + class_name).fadeOut(1000);
-                }, 2500);
-                this.count++;
-            }
-        }
-        // ===================End Toaster object
-
-
-    </script>
-
-    @stack('custom-js')
+  
 </body>
 
 </html>
