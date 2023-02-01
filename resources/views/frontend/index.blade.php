aa177c54f5a09c0e60b6f102dee9a2131b22ee6e
Akash Debnath <akash93734089@gmail.com>
2023-02-01 10:15:34 +0600

    full page design & responsive

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,139 +2,347 @@
 @push('custom-css')
 @endpush
 @section('page_conent')
+ <!--///////-header-section-start-///////-->
 
-<!-- HEADER-SECTION-START -->
-<div class="header-section">
+ <div class="header-section">
     <div class="container">
-        <h1>চুল পড়া নিয়ে চিন্তিত ?</h1>
+        <div class="header-main-column">
+            <h1 class="header-title">আপনি কি শারিরীক দুর্বলতায় ভোগছেন?</h1>
+        </div>
+        <h2 class="extra-title">মধুময় বাদাম মুহুর্তেই শারিরীক দুর্বলতা ও ক্লান্তিভাব দূর করবে পুষ্টিগুনে ভরপুর ৩৬ টি প্রিমিয়াম কোয়ালিটি এর বাদাম, ড্রাই ফ্রুটস, বীজ দিয়ে প্রস্তুতকৃত আমাদের এই প্রোডাক্ট।</h2>
+
     </div>
 </div>
-<!-- HEADER-SECTION-END -->
+                    <!--///////-header-section-End-///////-->
 
-<!-- VIDEO-SECTION-START -->
-<div class="header-section">
+
+                    <!--///////-video-section-start-///////-->
+
+<div class="video-section">
     <div class="container">
-        <h2>চুলের যে কোন সমস্যার প্রকৃতিক সমাধান পেতে <span>ভিডিওটি শেষ পর্যন্ত দেখুন..</span></h2>
-        <div class="video">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/QvbuDAaaQ6Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-        <div class="order-btn">
-            <a href="এখানে অর্ডার করুন"></a>
+        <div class="video-main-column">
+            <h2 class="video-title">মধুময় বাদাম এর উপকারিতা জানতে <span>নিচের সম্পূর্ণ ভিডিও দেখুন..</span></h2>
+            <div class="video-column">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/QvbuDAaaQ6Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+            </div>
         </div>
     </div>
-</div> 
-<!-- VIDEO-SECTION-END -->
+</div>
+                    <!--///////-video-section-End-///////-->
 
-<!-- BENEFIT-SECTION-START -->
-<div class="benefit-section">
+                    <!--///////-service-section-start-///////-->
+            
+<div class="service-section">
     <div class="container">
-        <h2 class="title">কেন এই কম্বো আপনার <span>ব্যবহার করা উচিত?</span></h2>
-        <div class="benefit-col-group">
-            <div class="benefit-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+        <div class="btn-column">
+            <a class="order-btn" href="#order-sec">এখানে অর্ডার করুন </a>
+        </div>
+        <div class="service-main-column">
+            <div class="service-title">
+            <h2> 
+                মধুময় বাদামে রয়েছে উন্নতমানের <span>৩৬ টি উপাদান,</span></h2>
             </div>
-            <div class="benefit-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            <div class="flex-column">
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> কাজুবাদাম</h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> কাঠবাদাম</h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> 
+                            পেস্তা বাদাম
+                            </h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> আখরোট</h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> 
+                            ড্রাই ম্যাংগো লাল
+                            </h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> 
+                            রক মেলন
+                            </h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> পামেলো</h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> 
+                            কাল সাকুরা
+                            </h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> 
+                            লাল সাকুরা
+                            </h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> 
+                            ড্রাই ম্যাংগো হলুদ
+                            </h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> 
+                            চেরি টমেটো
+                            </h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> চিনাবাদাম</h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> 
+                            গোল্ডেন কিসমিস
+                            </h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> 
+                            কালো কিসমিস
+                            </h2>
+                    </div>
+                </div>
+
+                <div class="service-single-column">
+                    <div class="content">
+                        <div class="service-img">
+                            <i class="fa-sharp fa-solid fa-circle-check"></i>
+                        </div>
+                        <h2> 
+                            ড্রাই আলুবোখারা
+                            </h2>
+                    </div>
+                </div>
             </div>
-            <div class="benefit-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+        </div>
+    </div>
+</div>
+
+
+                        <!--///////-service-section-End-///////-->
+
+                        <!--///////-sefty-device-section-start-///////-->
+
+<div class="sefty-device-section">
+<div class="container">
+    <div class="sefty-device-main">
+        <div class="flex-column">
+            <div class="product-column">
+                <img class="sefty-device-img" src="img/honey.jpg" alt="sefty-device">
             </div>
-            <div class="benefit-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            <div class="product-details-column">
+                <h2>Honey Nuts - <span>মধুময় বাদাম</span>
+                    এর উপকারিতা।</h2>
+                    <p>✔️ রক্তে কোলেস্টেরল কমানোর পাশাপাশি ক্যান্সার সৃষ্টি হওয়া থেকে বাধা দেয় এটি।</p>
+                    <p>✔️ শরীরের রোগ প্রতিরোধ ক্ষমতা বৃদ্ধিসহ হার্ট অ্যাটাক ও স্টোকের আশংকা কমায়। </p>
+                    <p>✔️ চেহারায় লাবণ্য ধরে রাখতে খুবই কার্যক</p>
+                    <p>✔️ যারা শরী’র ফি’ট রাখতে চান তাদের জন্য খুবই উ’পকারী</p>
+                    <p>✔️ স্মৃতিশক্তি বৃদ্ধি করে এবং তীক্ষ্ণ মেধা সম্পন্ন হতে সাহায্য করে।</p>
+                    <p>✔️ অকাল বা’র্ধক্য রোধে অত্যন্ত কার্যকর ভূমিকা রাখে।</p>
+                <div class="btn-column">
+                    <a class="order-btn" href="#order-sec"> এখানে অর্ডার করুন </a>
+                </div>
             </div>
-            <div class="benefit-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+        </div>
+    </div>
+</div>
+</div>
+                <!--///////-sefty-device-section-End-///////-->
+
+                    <!--///////-DELEVERY-section-START-///////-->
+<div class="delevery-charg-section">
+    <div class="container">
+        <h2 class="title">ডেলিভারি চার্জ</h2>
+        <div class="flex-column">
+            <div class="left-col">
+               <h2> ঢাকার ভিতরে- <span>৬০ টাকা</span></h2>
             </div>
-            <div class="benefit-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
-            </div>
-            <div class="benefit-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
-            </div>
-            <div class="benefit-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>গোড়া মজবুদ করে চুল পড়া ১০০% বন্ধ হবে।</h2></div>
+            <div class="left-col">
+               <h2> ঢাকার বাহিরে- <span> ১২০ টাকা</span></h2>
             </div>
             
         </div>
+    </div>
+</div>
+                     <!--///////-DELEVERY-section-End-///////-->
 
-        <div class="order-btn">
-            <a href="এখানে অর্ডার করুন"></a>
+                   <!--///////-why-choose-section-START-///////-->
+<div class="why-choose-section">
+<div class="container">
+    <h2 class="title">
+        কেন আমাদের পন্যটাই <span>অর্ডার করবেন ?</span></h2>
+    <div class="flex-column">
+        <div class="left-col">
+            <h2> ১. আমাদের পেজ এবং গ্রুপে পাবেন এই মধুময় বাদাম এর শতশত পজেটিভ রিভিউ আলহামদুলিল্লাহ, যারা হাতে পেয়ে চেক করে খেয়ে রিভিউগুলো দিয়েছেন।</h2>
+        </div>
+        <div class="left-col">
+            <h2> ২. যেহেতু পন্য হাতে পেয়ে চেক করে এমনকি খেয়ে মুল্য পরিশোধ করতে পারছেন, তাই নিশ্চিতে অর্ডার করতে পারেন।</span></h2>
+        </div>
+        
+    </div>
+</div>
+<div class="btn-column">
+    <a class="order-btn" href="#order-sec">এখানে অর্ডার করুন </a>
+</div>
+</div>
+                                     <!--///////-why-choose-section-End-///////-->
+
+                                      <!--///////-order-section-start-///////-->
+
+<div class="order-section" >
+    <div class="container" id="order-sec">
+        <div class="order-main" >
+            <h2 class="order-title">অর্ডার করতে আপনার সঠিক তথ্য দিয়ে নিচের ফর্মটি সম্পূর্ণ পূরন করুন। (আগে থেকে কোন টাকা দেয়া লাগবে না। প্রোডাক্ট হাতে পাবার পর টাকা দিবেন)</h2>
+            <div class="flex-column">
+                <div class="billing-details-column">
+                    <div class="billing-form">
+                        <h2 class="main-title">Billing details</h2>
+                    <form action="">
+                        <label for="name">আপনার সম্পূর্ন নাম: <abbr class="required" title="required">*</abbr></label><br>
+                        <input type="text" id="name" name="name" value="" placeholder="পুরো নাম" required ><br>
+                        <label for="address">আপনার ঠিকানা: <abbr class="required" title="required">*</abbr></label><br>
+                        <input type="text" id="address" name="address" value="" placeholder="বাসা নং, রোড নং, জেলা, থানা" required><br>
+                        <label for="phn-number">আপনার মোবাইল নাম্বার: <abbr class="required" title="required">*</abbr></label><br>
+                        <input type="tel" id="phn-number" name="phone" value="" placeholder="017xxxxxxxx" required><br><br>
+                    </form>
+                </div>
+                </div>
+                <div class="your-order-column">
+                    <h2 class="main-title">Your order</h2>
+                    <div class="order-cart">
+                        <table>
+                            <thead class="order-heading">
+                            <tr>   
+                                <th><h3>Product</h3></th>
+                                <th><h3>Subtotal</h3></th>
+                            </tr>
+                        </thead>
+                        <tbody class="order-details">
+                            <tr class="order-product">
+                                <td><span><img src="img/product-img01.jpg" alt=""></span><span>মধুময় বাদাম ৫০০ গ্রাম।</span></td>
+                                <td><h3><span>x1</span>500৳</h3></td>
+                            </tr>
+                            <tr class="total-bill">
+                                <td class="shipping-td"><h4>Subtotal</h4><h4>Shipping</h4></td>
+                                <td><h4>500৳</h4>
+                                    <input type="radio" id="" name="fav_language" value="">
+                                     <label for="html">ঢাকার ভিতরে-: <br> <span>60.00 ৳</span></label><br>
+                                    <input type="radio" id="" name="fav_language" value="">
+                                    <label for="html">ঢাকার বাহিরে-:<br> <span>120.00 ৳</span></label><br>
+                                </td>
+                            </tr>
+                            <tr class="total-bill">
+                                <td><h3>Total</h3></td>
+                                <td><h3>500৳</h3></td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </div>
+                    <div class="payment-column">
+                        <div class="payment-box">
+                           <h3>Cash on delivery</h3>
+                           <div class="pay"><h3>Pay with cash upon delivery.</h3></div>
+                        </div>
+                        <p>Your personal data will be used to process your order, support your experience throughout this website, and for other purposes described in our <span><a href="#">privacy policy</a></span>.</p>
+                        <div class="place-order">
+                            <a href="#">Place order <span>500৳</span></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
-<!-- BENEFIT-SECTION-END -->
+                    <!--///////-order-section-end-///////-->
 
-<!-- qualitative-elements-SECTION-START -->
-<div class="qualitative-section">
-    <div class="container">
-        <h2 class="title">যেসব গুনগত উপাদান দিয়ে এই <span>কম্বো তৈরী ?</span></h2>
-        <div class="qualitative-col-group">
-            <div class="qualitative-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>আমলকি</h2></div>
-            </div>
-            <div class="qualitative-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>আমলকি</h2></div>
-            </div>
-            <div class="qualitative-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>আমলকি</h2></div>
-            </div>
-            <div class="qualitative-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>আমলকি</h2></div>
-            </div>
-            <div class="qualitative-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>আমলকি</h2></div>
-            </div>
-            <div class="qualitative-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>আমলকি</h2></div>
-            </div>
-            <div class="qualitative-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>আমলকি</h2></div>
-            </div>
-            <div class="qualitative-item">
-                <div class="item-img"></div>
-                <div class="item-text"><h2>আমলকি</h2></div>
-            </div>
-        </div>
-            <div class="extra-title">
-                <h2>এইরকম ৪২টি উপাদান ব্যবহার করা হয় আমাদের এই কম্বোটি তৈরী করতে।</h2>
-            </div>
-        <div class="order-btn">
-            <a href="এখানে অর্ডার করুন"></a>
-        </div>
-    </div>
-</div>
-<!-- qualitative-elements-SECTION-END -->
 
-<!-- PRICE-SECTION-START -->
-<div class="price-section">
-    <div class="container">
-        <div class="main">
-            <h2>অফার মূল্য ​</h2>
-            <h3>৩টি বোতল একসাথে পাচ্ছেন ,</h3>
-            <h3>৳ ৯৫০/- <span><del>৳১৪৯৯/-</del></span></h3>
-            <h2>--ডেলিভারি চার্জ প্রযোজ্য--</h2>
-            <h3>সারাদেশে ক্যাশ অন হোম ডেলিভারি দেয়া হয়</h3>
-            <div class="order-btn">
-            <a href="এখানে অর্ডার করুন"></a>
-        </div>
-        </div>
-    </div>
-</div>
+
 @endsection
 @push('custom-js')
 
