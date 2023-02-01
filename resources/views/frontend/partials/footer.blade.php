749d9236c215712d10c2140eb7c57d514a35c0d2
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-02-01 12:59:33 +0600

    all js fie remove

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -44,54 +44,9 @@
 
                     <!--///////-footer-section-end-///////-->
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-</script>
-
-<script src="https://code.jquery.com/jquery-3.6.3.min.js"
-    integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-
   {{-- ================================================================================================
         =============================================  javascript  global function ========================
         =================================================================================================  --}}
 <script>
-    // ========================Language conversation
-    var bn2en = n => n.replace(/[০-৯]/g, d => "০১২৩৪৫৬৭৮৯".indexOf(d));
-    var en2bn = n => n.replace(/\d/g, d => "০১২৩৪৫৬৭৮৯" [d]);
-
-
-    //  ===================Toaster object
-    var toastr = {
-        count: 0,
-        toaster_append(msg, class_name) {
-            $('.toastr-div').append(`<div class="toastr ${class_name} mt-2">
-                                                <span class="toaster-msg">${msg}</span>
-                                            </div>`);
-        },
-        success(msg) {
-            let class_name = 'toastr' + this.count;
-            this.toaster_append(msg, class_name);
-
-            $('.' + class_name).fadeIn(1000);
-            setTimeout(() => {
-                $('.' + class_name).fadeOut(1000);
-            }, 2500);
-            this.count++;
-        },
-        error(msg) {
-            let class_name = 'toastr' + this.count;
-            this.toaster_append(msg, class_name);
-
-            $('.' + class_name).css({
-                backgroundColor: 'red'
-            });
-            $('.' + class_name).fadeIn(1000);
-            setTimeout(() => {
-                $('.' + class_name).fadeOut(1000);
-            }, 2500);
-            this.count++;
-        }
-    }
-    // ===================End Toaster object
+    
 </script>
